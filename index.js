484cde30fb8f7f8fee49e74500d8133005d70f73
thaim <thaim24@gmail.com>
2021-07-23 23:03:29 +0900

    print pull request data using octokit

--- index.js
+++ index.js
@@ -12,6 +12,15 @@ async function run() {
       apiKey: process.env.REDMINE_APIKEY
     };
     const redmine = new Redmine(hostname, config);
+    const context = github.context;
+    const octokit = github.getOctokit(${{ secrets.GITHUB_TOKEN }})
+
+    const pr = await octokit.rest.pulls.get({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      pull_number: context.payload.pull_request.number
+    });
+    const redmine_issue_numbers = await parse_redmine_issues(pr.data);
 
     redmine.get_issue_by_id(776, null, function(err, data) {
       if (err) throw err;
@@ -21,6 +30,11 @@ async function run() {
   } catch (error) {
     console.error("error: " + error);
   }
+}
+
+async function parse_redmine_issues(prdata) {
+  console.log(JSON.stringify(prdata));
+  return 776;
 }
 
 run();
